c9015ae906682911b3a9def240f354cc3fe4fe7c
Valentine Tury <mrjf009@gmail.com>
2022-06-21 17:42:35 +0200

    Designed login component

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  usernameFormControl = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  matcher = new MyErrorStateMatcher();
+  constructor(public authService: AuthService) {}
+  ngOnInit() {}
 }
