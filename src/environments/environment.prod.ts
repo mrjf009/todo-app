5a7623c9a487cbdd8ac393121cf834e25f5e6c40
Valentine Tury <mrjf009@gmail.com>
2022-06-22 18:12:55 +0200

    Updated firebase data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-de0f7',
+    appId: '1:633752626483:web:3ecdceb8256115d1700a46',
+    databaseURL: 'https://todo-app-de0f7-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'todo-app-de0f7.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBD9plz334lAkBLbJKdWvi-fX45uGaZfD8',
+    authDomain: 'todo-app-de0f7.firebaseapp.com',
+    messagingSenderId: '633752626483',
+    measurementId: 'G-M4ZD0CSXGC',
+  },
   production: true
 };
